e62d35a6adf95f0ae717f2a0f84d173565b0796c
Joel Roske <joel.j.roske@gmail.com>
2020-09-25 11:53:28 -0500

    convert Square comp to func with hook setValue

--- src/index.js
+++ src/index.js
@@ -1,47 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Square(props) {
+function Square({number}) {
+  const [value, setValue] = useState('')
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button 
+    className="square" 
+    onClick={() => setValue('X')}
+    >
+      {value}
     </button>
   );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
+function Board () {
+  function renderSquare(i) {
     return (
       <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
       />
     );
   }
 
-  render() {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     );
   }
-}
+
 
 class Game extends React.Component {
   constructor(props) {
