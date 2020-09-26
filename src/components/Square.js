9eb57d98af451aa2873d21fe6154fb90dd12a843
Joel Roske <joel.j.roske@gmail.com>
2020-09-26 15:07:35 -0500

    destructured props in Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 
-const Square = () => <div>Square</div>
+const Square = ({ value, onClick }) => (
+<button className="square" onClick={onClick}>
+      {value}
+    </button>
+    )
 
 export default Square;
